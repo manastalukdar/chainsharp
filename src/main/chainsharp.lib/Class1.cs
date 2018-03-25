e5b38a0aac13cf0fc2f125a3c3dc36c8b98b5719
Manas Talukdar <manas.talukdar@gmail.com>
2018-03-25 01:04:20 -0700

    Fixed gitignore.

---
+++ src/main/chainsharp.lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chainsharp.lib
+{
+    public static class Class1
+    {
+        public static string dummy() 
+        {
+            return Environment.CurrentDirectory;
+        }
+    }
+}
